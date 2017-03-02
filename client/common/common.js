2f5a43be0a9923cbc1f44a0a508c04c152035714
Czl <471060995@qq.com>
2017-03-02 16:48:12 +0800

    boom

--- client/common/common.js
+++ client/common/common.js
@@ -6,13 +6,3 @@ const log = function (msg) {
     window.vm && (window.vm.logs.push(msg));
 };
 
-const trampoline = async function (func, arg) {
-    var value = await func(arg);
-    console.info(func)
-    console.info(typeof value);
-    while (typeof value == 'function') {
-        value = await value();
-    }
-    return value;
-};
-

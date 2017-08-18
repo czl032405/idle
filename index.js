d0903b541b09b60bd792bd5d6f2f0d6462ff233c
HUCAI\09057414 <471060995@qq.com>
2017-08-18 15:18:48 +0800

    boom

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-var pmx = require("pmx");
-var spawn = require('child_process').spawn;
-pmx.action('client:build', function (reply) {
-    var child = spawn('npm', ['run','build'],{cwd:"./idle-client"});
-    child.stdout.on('data', function (chunk) {
-        chunk.toString().split('\n').forEach(function (line) {
-            res.send(line); 
-        });
-    });
-    child.stdout.on('end', function (chunk) {
-        res.end('end'); 
-    });
-    child.on('error', function (e) {
-        res.error(e);  
-    });
+// var pmx = require("pmx");
+// var spawn = require('child_process').spawn;
+// pmx.action('client:build', function (reply) {
+//     var child = spawn('npm', ['run','build'],{cwd:"./idle-client"});
+//     child.stdout.on('data', function (chunk) {
+//         chunk.toString().split('\n').forEach(function (line) {
+//             res.send(line); 
+//         });
+//     });
+//     child.stdout.on('end', function (chunk) {
+//         res.end('end'); 
+//     });
+//     child.on('error', function (e) {
+//         res.error(e);  
+//     });
 
-})
+// })
 
-// require('./idle-server/dist');
+require('./idle-server/dist');

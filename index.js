9e23f9ecab45ccb9bfb3ea9e6131ca888d6f8d3c
HUCAI\09057414 <471060995@qq.com>
2017-08-18 15:54:26 +0800

    boom

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 var pmx = require("pmx");
 var spawn = require('child_process').spawn;
 pmx.action('client:build', function (reply) {
-    var child = spawn('npm', ['run', 'build'], { cwd: "./idle-client", stdio: "inherit"});
-    child.on('close',function(){
+    var child = spawn('npm', ['run', 'build'], { cwd: "./idle-client", stdio: "inherit" });
+    child.on('close', function () {
         console.info('boom');
-        reply({boom:true})
+        reply({ boom: true })
+    })
+    child.on('error', function (e) {
+        reply(e)
     })
 })
 
 pmx.action('server:build', function (reply) {
-    var child = spawn('npm', ['run', 'build'], { cwd: "./idle-server", stdio: "inherit"});
-    child.on('close',function(){
+    var child = spawn('npm', ['run', 'build'], { cwd: "./idle-server", stdio: "inherit" });
+    child.on('close', function () {
         console.info('boom');
-        reply({boom:true})
+        reply({ boom: true })
+    })
+    child.on('error', function (e) {
+        reply(e)
     })
 })
 

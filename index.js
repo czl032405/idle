3c33e111feccf7504be2cf433176f515f9571279
HUCAI\09057414 <471060995@qq.com>
2017-08-18 15:53:36 +0800

    boom

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ var spawn = require('child_process').spawn;
 pmx.action('client:build', function (reply) {
     var child = spawn('npm', ['run', 'build'], { cwd: "./idle-client", stdio: "inherit"});
     child.on('close',function(){
-        console.info('boom')
+        console.info('boom');
+        reply({boom:true})
     })
 })
 
 pmx.action('server:build', function (reply) {
     var child = spawn('npm', ['run', 'build'], { cwd: "./idle-server", stdio: "inherit"});
     child.on('close',function(){
-        console.info('boom')
+        console.info('boom');
+        reply({boom:true})
     })
 })
 

120898ddf5e16f3098dde4b802e9523b9fea7843
HUCAI\09057414 <471060995@qq.com>
2017-08-18 15:14:23 +0800

    boom

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,19 +2,10 @@ module.exports = {
   apps: [
     {
       name: 'idle',
-      cwd: "./idle-server",
-      script: 'npm',
-      args:"start"
-    },
-  ],
-  deploy: {
-    production: {
-      user: 'node',
-      host: '212.83.163.1',
-      ref: 'origin/master',
-      repo: 'git@github.com:repo.git',
-      path: '/var/www/production',
-      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production'
+      script: "./index.js",
+      env:{
+        port:9001
+      },
     }
-  }
+  ]
 };
--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-require('./idle-server/dist')
+var pmx = require("pmx");
+var spawn = require('child_process').spawn;
+pmx.action('client:build', function (reply) {
+    var child = spawn('npm', ['run','build'],{cwd:"./idle-client"});
+    child.stdout.on('data', function (chunk) {
+        chunk.toString().split('\n').forEach(function (line) {
+            res.send(line); 
+        });
+    });
+    child.stdout.on('end', function (chunk) {
+        res.end('end'); 
+    });
+    child.on('error', function (e) {
+        res.error(e);  
+    });
+
+})
+
+// require('./idle-server/dist');

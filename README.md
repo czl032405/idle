32f71d28e25b193bfd034c0aa4be102d1e4dff94
HUCAI\09057414 <471060995@qq.com>
2017-09-21 12:31:47 +0800

    boom

---
+++ README.md
@@ -0,0 +1,14 @@
+# IDLE
+
+```bash
+#dev
+mongod
+cd idle-server && npm run dev
+cd idle-client && npm run watch 
+
+#dev-only-client, proxy to idle.my-egg.me/api
+cd idle-client && npm run dev 
+
+#pro
+cd idle-server && npm run start 
+```
--- index.js
+++ index.js
@@ -1,48 +1,110 @@
-var pmx = require("pmx");
-var spawn = require('child_process').spawn;
-pmx.action('client:build', function (reply) {
-    var child = spawn('npm', ['run', 'build'], { cwd: "./idle-client", stdio: "inherit" });
-    child.on('close', function () {
-        console.info('boom');
-        reply({ boom: true })
-    })
-    child.on('error', function (e) {
-        reply(e)
-    })
-})
-pmx.action('client:install', function (reply) {
-    var child = spawn('npm', ['install'], { cwd: "./idle-client", stdio: "inherit" });
-    child.on('close', function () {
-        console.info('boom');
-        reply({ boom: true })
-    })
-    child.on('error', function (e) {
-        reply(e)
-    })
-})
+const pmx = require("pmx");
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 
-pmx.action('server:build', function (reply) {
-    var child = spawn('npm', ['run', 'build'], { cwd: "./idle-server", stdio: "inherit" });
-    child.on('close', function () {
-        console.info('boom');
-        reply({ boom: true })
-    })
-    child.on('error', function (e) {
-        reply(e)
-    })
-})
 
-pmx.action('server:install', function (reply) {
-    var child = spawn('npm', ['install'], { cwd: "./idle-server", stdio: "inherit" });
-    child.on('close', function () {
-        console.info('boom');
-        reply({ boom: true })
-    })
-    child.on('error', function (e) {
-        reply(e)
-    })
-})
 
+// pmx.action('client:build', function (reply) {
+//     var child = spawn('npm', ['run', 'build'], { cwd: "./idle-client", stdio: "inherit" });
+//     child.on('close', function () {
+//         console.info('boom');
+//         reply({ boom: true })
+//     })
+//     child.on('error', function (e) {
+//         reply(e)
+//     })
+// })
+// pmx.action('client:install', function (reply) {
+//     var child = spawn('npm', ['install'], { cwd: "./idle-client", stdio: "inherit" });
+//     child.on('close', function () {
+//         console.info('boom');
+//         reply({ boom: true })
+//     })
+//     child.on('error', function (e) {
+//         reply(e)
+//     })
+// })
+
+// pmx.action('server:build', function (reply) {
+//     var child = spawn('npm', ['run', 'build'], { cwd: "./idle-server", stdio: "inherit" });
+//     child.on('close', function () {
+//         console.info('boom');
+//         reply({ boom: true })
+//     })
+//     child.on('error', function (e) {
+//         reply(e)
+//     })
+// })
+
+// pmx.action('server:install', function (reply) {
+//     var child = spawn('npm', ['install'], { cwd: "./idle-server", stdio: "inherit" });
+//     child.on('close', function () {
+//         console.info('boom');
+//         reply({ boom: true })
+//     })
+//     child.on('error', function (e) {
+//         reply(e)
+//     })
+// })
+
+
+//todo 
+var installClient = async function (reply) {
+    reply = reply || function (e) {console.error(e) };
+    try {
+        reply('delete package-lock.json');
+        var { stdout, stderr } = await exec('rm -rf package-lock.json', { cwd: "./idle-client" });
+        reply(stdout)
+        reply(stderr)
+
+
+        reply('begin: npm install in ./idle-client')
+        var { stdout, stderr } = await exec('npm install', { cwd: "./idle-client" });
+        reply(stdout)
+        reply(stderr)
+        reply('end')
+
+        reply('begin: npm run build in ./idle-client')
+        var { stdout, stderr } = await exec('npm run build', { cwd: "./idle-client" });
+        reply(stdout)
+        reply(stderr)
+        reply('end')
+
+    } catch (error) {
+        reply(error);
+    }
+}
+
+
+var installServer = async function (reply) {
+    reply = reply || function (e) {console.error(e) };
+    try {
+        reply('delete package-lock.json');
+        var { stdout, stderr } = await exec('rm -rf package-lock.json', { cwd: "./idle-server" });
+        reply(stdout)
+        reply(stderr)
+
+        reply('begin: npm install in ./idle-server')
+        var { stdout, stderr } = await exec('npm install', { cwd: "./idle-server" });
+        reply(stdout)
+        reply(stderr)
+        reply('end')
+
+        reply('begin: npm run build in ./idle-server')
+        var { stdout, stderr } = await exec('npm run build', { cwd: "./idle-server" });
+        reply(stdout)
+        reply(stderr)
+        reply('end')
+
+    } catch (error) {
+        reply(error);
+    }
+}
+
+
+pmx.action('client:install',installClient)
+
+pmx.action('server:install',installServer);
 
 require('./idle-server/dist');
 
